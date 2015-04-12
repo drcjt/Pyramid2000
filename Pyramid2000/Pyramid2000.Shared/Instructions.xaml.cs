05554a6d8f91a21fbaafb93b2c28633e13110d81
drcjt <drcjt@hotmail.com>
2015-04-12 17:00:53 +0100

    Change instructions page label to "help"

--- Pyramid2000/Pyramid2000.Shared/Instructions.xaml.cs
+++ Pyramid2000/Pyramid2000.Shared/Instructions.xaml.cs
@@ -114,7 +114,7 @@ namespace Pyramid2000
         private void InsertPageTitleUserControl()
         {
             PageTitleUserControl pageTitle = new PageTitleUserControl(this.navigationHelper);
-            pageTitle.PageTitle = "instructions";
+            pageTitle.PageTitle = "help";
             pageTitle.AppName = "PYRAMID 2000";
             LayoutRoot.Children.Add(pageTitle);
             Grid.SetRow(pageTitle, 0);
