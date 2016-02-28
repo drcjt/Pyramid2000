a145259e2d3513780b8f2cd41059876e7d1dd592
drcjt <drcjt@hotmail.com>
2016-02-28 21:58:22 +0000

    Add test to cover Saving game state

--- Pyramid2000EngineTests/GameTests.cs
+++ Pyramid2000EngineTests/GameTests.cs
@@ -66,5 +66,30 @@ namespace Pyramid2000EngineTests
             Mock.Get(printer).Verify(p => p.PrintLn(resources.Room1));
             Mock.Get(printer).Verify(p => p.Print(resources.Prompt));
         }
+
+        [Test]
+        public void Save_WhenGameIsInInitialState_ResultsInInitialStateSerialised()
+        {
+            // Arrange
+            var resources = new Resources();
+            var settings = new GameSettings();
+            var printer = new Mock<IPrinter>().Object;
+            var items = new Items(resources);
+            var player = new Player(items);
+            player.CurrentRoom = "room_1";
+            var parser = new Parser(player, printer, items, settings, resources);
+            var rooms = new Rooms(items, resources);
+            var gameState = new GameState();
+            var scripter = new Scripter(printer, items, rooms, player, gameState, settings, resources);
+            var defaultScripter = new DefaultScripter(resources);
+
+            var game = new Game(player, printer, parser, scripter, rooms, defaultScripter, items, gameState, resources);
+
+            // Act
+            var result = game.State;
+
+            // Assert
+            Assert.AreEqual("LOAD ,,,,,_51,_81,,,,_16,,,_2,,_8,_9,_72,_11,,,,_61,,_59,_2,_2,#BOTTLE,,_56,_76,,_73,_68,,,_14,_17,_25,_18,_24,,_71,,room_1,,0,False,310", result);
+        }
     }
 }
