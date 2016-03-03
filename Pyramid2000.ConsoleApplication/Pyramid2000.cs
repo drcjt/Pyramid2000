396c3a7e7745467a3aa46f9b92c0d2db671ddb7b
drcjt <drcjt@hotmail.com>
2016-03-03 21:03:55 +0000

    Add simple command line argument parser and use to support basic game settings for console application

--- Pyramid2000.ConsoleApplication/Pyramid2000.cs
+++ Pyramid2000.ConsoleApplication/Pyramid2000.cs
@@ -14,29 +14,56 @@ namespace Pyramid2000.ConsoleApplication
     {
         static void Main(string[] args)
         {
-            IResources resources = new Resources();
-            IGameSettings settings = new GameSettings();
-            IPrinter printer = new Printer(new ConsolePrinter(), settings);
-            IItems items = new Items(resources);
-            IPlayer player = new Player(items);
-            player.CurrentRoom = "room_1";
-            IParser parser = new Parser(player, printer, items, settings, resources);
-            IRooms rooms = new Rooms(items, resources);
-            IGameState gameState = new GameState();
-            IScripter scripter = new Scripter(printer, items, rooms, player, gameState, settings, resources);
-            IDefaultScripter defaultScripter = new DefaultScripter(resources);
-
-            IGame game = new Game(player, printer, parser, scripter, rooms, defaultScripter, items, gameState);
-
-            game.Init();
-
-            while (!gameState.GameOver)
+            // Show usage with details of command line options
+            if (args.Length == 1 && args[0] == "/?")
             {
-                var input = Console.ReadLine();
-                game.ProcessPlayerInput(input);
+                Console.WriteLine("Usage: Pyramid2000.ConsoleApplication [options]");
+                Console.WriteLine();
+                Console.WriteLine("Pyramid 2000 Options");
+                Console.WriteLine();
+                Console.WriteLine("Allcaps={true|false}               Specifies if all captials should be used");
+                Console.WriteLine("Trs80Mode={true|false}             Specifies if exact TRS-80 mode should be used");
             }
+            else
+            {
+                // Setup all of the game's dependencies
+                var resources = new Resources();
+                var settings = new GameSettings();
+                var printer = new Printer(new ConsolePrinter(), settings);
+                var items = new Items(resources);
+                var player = new Player(items);
+                player.CurrentRoom = "room_1";
+                var parser = new Parser(player, printer, items, settings, resources);
+                var rooms = new Rooms(items, resources);
+                var gameState = new GameState();
+                var scripter = new Scripter(printer, items, rooms, player, gameState, settings, resources);
+                var defaultScripter = new DefaultScripter(resources);
 
-            Console.ReadLine();
+                // Parse the command line arguments
+                var cmdLineArgs = new CommandLineArgs();
+                cmdLineArgs.ParseArgs(args, "allcaps=true;trs80mode=true");
+
+                // Initialise the settings based on the command line arguments/default arugments
+                settings.AllCaps = cmdLineArgs.AsBool("allcaps");
+                settings.Trs80Mode = cmdLineArgs.AsBool("trs80mode");
+
+                // Create the game
+                var game = new Game(player, printer, parser, scripter, rooms, defaultScripter, items, gameState);
+
+                // Initialise the game
+                game.Init();
+
+                // While the game isn't over keep playing
+                while (!gameState.GameOver)
+                {
+                    // Read the player's input and process it
+                    var input = Console.ReadLine();
+                    game.ProcessPlayerInput(input);
+                }
+
+                // Game over - give the player a chance to read any final messages before quitting
+                Console.ReadLine();
+            }
         }
     }
 }
